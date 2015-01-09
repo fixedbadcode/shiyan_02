75d35d11f7252192cd9a624d2e44079941122bf6
jxjgssylsg <jxjgssylsg@126.com>
2015-01-09 22:52:00 +0800

    i just want to know why

--- shiyan_github_01.cpp
+++ shiyan_github_01.cpp
@@ -6,9 +6,9 @@
 #include <iostream>
 using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
-{  sdafasdfads
-    for 1.8 23.29
-	sdfsdfsdf
+{  sdafasdfads   
+    for 1.8 23.29   //today is 1,9 22.50
+	sdfsdfsdf 
     //i cant figure it out
 	cin.get();
 	return 0;
